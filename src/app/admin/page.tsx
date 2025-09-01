7c261fe59ad5ee381280d3a6bffafefdfda578a5
ms-tristan <tristan.masse@epitech.eu>
2025-09-01 17:38:56 +0200

    [FEAT](frontend): made manyimprovement on file structure and created few useful ui components

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import React from 'react'
+
+export default function AdminDashboard() {
+    return (
+        <div className='flex flex-1 justify-center items-center'>Admin Dashboard</div>
+    )
+}
