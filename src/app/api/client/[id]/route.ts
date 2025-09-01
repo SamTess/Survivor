22676157261bf358260840d0ef42fda005504911
Samuel Tesson <samuel.tesson@epitech.eu>
2025-09-01 17:23:57 +0200

    [FIX](route.ts): fix user ID retrieval in the GET function

--- src/app/api/client/[id]/route.ts
+++ src/app/api/client/[id]/route.ts
@@ -3,11 +3,11 @@ import { userService } from "../../../../composition/container";
 
 export async function GET(
   _req: Request,
-  { params }: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
+  const { id } = await context.params;
   try {
-    const id = Number(params.id);
-    const user = await userService.getUserById(id);
+    const user = await userService.getUserById(Number(id));
     if (!user) return NextResponse.json({ error: "Not found" }, { status: 404 });
     return NextResponse.json(user);
   } catch (error) {
