688c67a7e8089aa3586774d2972b28bd9a4d7441
Samuel Tesson <samuel.tesson@epitech.eu>
2025-09-02 16:05:30 +0200

    [FEAT](sync): refactor repositories and services for external data synchronization

--- src/infrastructure/repositories/StartupRepository.ts
+++
@@ -1,7 +0,0 @@
-import { StartupDetailApiResponse, StartupListApiResponse, StartupFounder } from "../../domain/interfaces/Startup";
-
-export interface StartupRepository {
-  upsertList(item: StartupListApiResponse): Promise<void>;
-  upsertDetail(item: StartupDetailApiResponse): Promise<void>;
-  upsertFounders(founders: StartupFounder[], startupId: number): Promise<void>;
-}
