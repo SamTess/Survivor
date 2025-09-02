f9a42a730a28827ea75999c3ddd9efc8f22a22f4
Samuel Tesson <samuel.tesson@epitech.eu>
2025-09-02 18:13:23 +0200

    [FEAT](sync/images): add image synchronization methods and update sync scheduler options

--- src/infrastructure/persistence/prisma/sync/StartupRepositoryPrisma.ts
+++ src/infrastructure/persistence/prisma/sync/StartupRepositoryPrisma.ts
@@ -105,4 +105,8 @@ export class StartupRepositoryPrisma implements StartupRepository {
   async saveImage(startupId: number, data: Buffer): Promise<void> {
     await prisma.s_STARTUP.update({ where: { id: startupId }, data: { image_data: data } });
   }
+
+  async saveImage(startupId: number, data: Buffer): Promise<void> {
+    await prisma.s_STARTUP.update({ where: { id: startupId }, data: { image_data: data } });
+  }
 }
