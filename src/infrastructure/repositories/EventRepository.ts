333fe63803ffecc045b365261d21271a52acacdc
Samuel Tesson <samuel.tesson@epitech.eu>
2025-09-02 16:05:30 +0200

    [FEAT](sync): refactor repositories and services for external data synchronization

--- src/infrastructure/repositories/EventRepository.ts
+++
@@ -1,6 +0,0 @@
-import { EventApiResponse } from "../../domain/interfaces/Event";
-
-export interface EventRepository {
-  upsert(item: EventApiResponse): Promise<void>;
-  saveImage(eventId: number, data: Buffer): Promise<void>;
-}
