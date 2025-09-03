66843cfeed3c420afedd2f9c82af77307400b11d
ms-tristan <tristan.masse@epitech.eu>
2025-09-03 15:13:57 +0200

    [FEAT](navbar, homepage): fixed navbar and homepage styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AdminSidebar from "@/components/navigation/AdminSidebar";
-import { Navigation } from "@/components/navigation";
+import { Navigation } from "@/components/navigation/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
