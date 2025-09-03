4415fa72325342c7ccf826d8a1cd2d1324ef0425
ctalneau <clement.talneau@epitech.eu>
2025-09-03 16:48:41 +0200

    [CI](New optimisation): With the help of Hugo i found new optimisation axis that will shorten the CI run time by like 20-30 seconds i think

--- scripts/start-prod.sh
+++ scripts/start-prod.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-echo "Waiting for database to be ready..."
-while ! nc -z db 5432; do
-  sleep 1
-done
-echo "Database is ready!"
-
 if [ -d "prisma/migrations" ] && [ "$(ls -A prisma/migrations 2>/dev/null)" ]; then
   echo "Running Prisma migrations..."
   npx prisma migrate deploy
